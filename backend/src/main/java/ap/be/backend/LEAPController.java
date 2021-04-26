a0197bbb5422a44586f5d44fbf042ba250551b9c
Jonas Staes (@s1018159) <s108159@ap.be>
2021-04-26 14:18:32 +0200

    Added capability code
    Added some basic properties to capability, created repository and route to view all capabilities

--- backend/src/main/java/ap/be/backend/LEAPController.java
+++ backend/src/main/java/ap/be/backend/LEAPController.java
@@ -1,8 +1,23 @@
 package ap.be.backend;
 
-import org.springframework.stereotype.Controller;
 
-@Controller
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import ap.be.backend.jpa.Capability;
+import ap.be.backend.jpa.CapabilityRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
 public class LEAPController {
+    
+    @Autowired
+    private CapabilityRepository capabilityRepository;
+
+    @GetMapping("/")
+    public Iterable<Capability> readCapabilities() {
+        return capabilityRepository.findAll();
+    }
     
 }
--- backend/src/main/java/ap/be/backend/jpa/Capability.java
+++ backend/src/main/java/ap/be/backend/jpa/Capability.java
@@ -12,5 +12,57 @@ public class Capability {
     @GeneratedValue
     private Long id;
 
-    
+    @Column
+    private String name;
+
+    @Column
+    private String description;
+
+    @Column
+    private int level = 1;
+
+    @Column
+    private Capability parent = null;
+
+    public Capability(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public Capability(String name, String description, Capability parent) {
+        this(name, description);
+        this.level = parent.getLevel() + 1;
+        this.parent = parent;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public Capability getParent() {
+        return parent;
+    }
+
+    public void setParent(Capability parent) {
+        this.parent = parent;
+    }
 }
