7982fdd4e4776d701f8009bc2e2c83bfeb96ce46
Abdel <s109434@ap.be>
2021-05-20 21:41:00 +0200

    onnodige code verwijderd

--- frontend/src/components/DatePicker.js
+++ frontend/src/components/DatePicker.js
@@ -10,24 +10,16 @@ import {
 } from '@material-ui/pickers';
 
 export default function DatePicker(props) {
-    // The first commit of Material-UI
 
     const [selectedDate, setSelectedDate] = React.useState(new Date().toJSON().slice(0, 10));
 
     const formatDate = (date) => {
         try {
-            if (date === null || date === undefined) {
-
-                setSelectedDate(date);
-                console.log("null: " + date);
-            } else {
-
-                let year = date.getFullYear();
-                let month = date.getMonth();
-                let day = date.getDate();
-                let fullDate = format(new Date(year, month, day), 'dd/MM/yyyy') //year + '-' + (month + 1) + '-' + day;
-                return fullDate;
-            }
+            let year = date.getFullYear();
+            let month = date.getMonth();
+            let day = date.getDate();
+            let fullDate = format(new Date(year, month, day), 'dd/MM/yyyy') //year + '-' + (month + 1) + '-' + day;
+            return fullDate;
         } catch (error) {
             console.error(error);
         }
@@ -36,6 +28,7 @@ export default function DatePicker(props) {
     const handleDateChange = (date) => {
         console.log("1: " + date);
         setSelectedDate(date);
+        console.log("selectedDate state: " + selectedDate);
         console.log("handleDate change " + formatDate(date));
         props.date(formatDate(date));
     };
